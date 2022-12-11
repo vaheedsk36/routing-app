c3d6119c1087d2d41d659c9c8aa1b575146bdb97
MASOOD AKHTAR VAHEED <masoodakhtarvaheed@MASOODs-MacBook-Air.local>
2022-12-12 02:36:35 +0530

    added-post-request

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const route = require("./router");
 const port = process.env.PORT || 2022;
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/api',route);
 
 app.get('/',(req,res)=>{
--- router.js
+++ router.js
@@ -2,8 +2,17 @@ const express = require("express");
 const route = express.Router();
 const accounts = require("./database");
 
+// GET request
 route.get('/accounts',(req,res)=>{
     res.json({userData:accounts});
+})
+
+// POST request
+route.post('/accounts',(req,res)=>{
+    const incomingAccount = req.body;
+    accounts.push(incomingAccount);
+    console.log(accounts);
+    res.json(accounts);
 })
 
 module.exports = route;
